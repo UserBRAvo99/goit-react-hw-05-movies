38c22acb858661496ea07900b78695c117702749
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-03-18 23:57:00 +0200

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,23 @@
 // import style from '../components/app.module.scss';
 
+import { Route, Routes, Link } from 'react-router-dom';
+
+import Home from './Home';
+import Movies from './Movies';
+
 export const App = () => {
   return (
     <div className="App">
       <header>
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">Movies</a>
-          </li>
-        </ul>
+        <div>
+          <Link to="/">Home</Link>
+          <Link to="/movies">Movies</Link>
+        </div>
       </header>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/movies" element={<Movies />}></Route>
+      </Routes>
     </div>
   );
 };
