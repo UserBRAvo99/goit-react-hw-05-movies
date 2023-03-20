8dc85986db5dc37eb0a107e41da54e98f3d8dffc
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-03-20 17:00:18 +0200

    commit

--- src/components/MoviesDetails/MoviesDetails.jsx
+++ src/components/MoviesDetails/MoviesDetails.jsx
@@ -6,37 +6,50 @@ import { useEffect } from 'react';
 import { getMovieId } from 'components/fetchId';
 
 function MoviesDetails() {
-  const [film, setFilm] = useState([]);
+  const [film, setFilm] = useState(null);
+  //   const [srcImg, setSrcImg] = useState('');
   const { moviesId } = useParams();
 
   useEffect(() => {
     const getData = async () => {
       try {
         const { data } = await getMovieId(moviesId);
+        console.log(data);
         setFilm(data);
+        // setSrcImg(data.poster_path);
       } catch (error) {
         throw new Error(error);
       }
     };
     getData();
   }, [moviesId]);
-
+  //   const src = `https://image.tmdb.org/t/p/w500${film.profile_path}`;
   return (
     <div>
-      <ul>
-        <li>
-          <h3>{film.title}</h3>
-          <p></p>
-        </li>
-        <li>
-          <h4>Overview</h4>
-          <p></p>
-        </li>
-        <li>
-          <h5>Genres</h5>
-          {film === [] && console.log(film.genres.map(e => console.log(e)))}
-        </li>
-      </ul>
+      {' '}
+      {film && (
+        <>
+          <img
+            src={`https://image.tmdb.org/t/p/w500${film.poster_path}`}
+            alt="img"
+          />
+          <ul>
+            <li>
+              <h3>{film?.title}</h3>
+              <p></p>
+            </li>
+            <li>
+              <h4>Overview</h4>
+              <p></p>
+            </li>
+            <li>
+              <h5>Genres</h5>
+              {film?.genres?.length &&
+                console.log(film?.genres.map(item => item.name))}
+            </li>
+          </ul>
+        </>
+      )}
     </div>
   );
 }
