f7b47eb4e729d89513903891bf6cc5cb451aca33
UserBRAvo99 <108820828+UserBRAvo99@users.noreply.github.com>
2023-03-21 17:18:36 +0200

    commit btn next and back

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 
 import Header from 'components/Header';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigate } from 'react-router-dom';
 
 function Layout() {
+  const navigate = useNavigate();
+  const handleClickBack = () => {
+    navigate(-1);
+  };
+  const handleClickNext = () => {
+    navigate(+1);
+  };
   return (
     <>
       <Header />
+      <div>
+        <button onClick={handleClickBack}>Go back</button>
+        <button onClick={handleClickNext}>Go next</button>
+      </div>
       <div>
         <Outlet />
       </div>
